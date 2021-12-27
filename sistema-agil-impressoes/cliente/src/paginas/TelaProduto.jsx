14b71d488f55a1143b31cd70c19529dad1eaeee1
antonioerivanio <antonioerivanio@gmail.com>
2021-12-27 16:06:34 -0300

    trabalhando na tela de produto

--- sistema-agil-impressoes/cliente/src/paginas/TelaProduto.jsx
+++ sistema-agil-impressoes/cliente/src/paginas/TelaProduto.jsx
@@ -1,5 +1,60 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Avaliacao from '../componentes/Avaliacao';
+import dadosProdutos from '../utils/dadosProdutos';
 
 export default function TelaProduto() {
-  return <div>Produto</div>;
+  let { id } = useParams();
+  const produto = dadosProdutos.produtos.find((x) => x._id === id);
+  if (!produto) return <div>Produto Não Encontrado!</div>;
+
+  return (
+    <div>
+      <div className="row top">
+        <div className="col-2">
+          <img src={produto.image} alt={produto.image} className="large" />
+        </div>
+        <div className="col-1">
+          <ul>
+            <li>{produto.nome}</li>
+            <li>
+              <Avaliacao
+                avaliacao={produto.avaliacao}
+                numVisualizacoes={produto.numVisualizacoes}
+              ></Avaliacao>
+            </li>
+            <li>Preço: ${produto.preco}</li>
+            <li>Descrição: {produto.descricao}</li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <div className="row">
+                  <div>Preço</div>
+                  <div className="price">R${produto.preco}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Status</div>
+                  <div className="price">
+                    {produto.quantidadeEmEstoque > 0 ? (
+                      <span className="sucesso">'Em estoque' </span>
+                    ) : (
+                      <span className="erro">'Produto indisponível'</span>
+                    )}
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button className="primary block">Adicionar ao carrinho</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
