b68e325ba68f606d91c63ab098bf2dbcb7c87c90
antonioerivanio <antonioerivanio@gmail.com>
2021-12-28 18:11:39 -0300

    instalando e configurando nodemon

--- sistema-agil-impressoes/cliente/src/paginas/TelaProduto.jsx
+++ sistema-agil-impressoes/cliente/src/paginas/TelaProduto.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Avaliacao from '../componentes/Avaliacao';
+
 import dadosProdutos from '../../../servidor/dadosProdutos';
 
 export default function TelaProduto() {
