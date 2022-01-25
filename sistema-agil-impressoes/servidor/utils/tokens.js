bfa3868a964aae2e32cd9ad56ccc3e0d64c90e41
antonio erivanio <antonioerivanio@gmail.com>
2022-01-25 15:37:29 -0300

    criado aqui para gerar tokens

---
+++ sistema-agil-impressoes/servidor/utils/tokens.js
@@ -0,0 +1,11 @@
+export const gerarToken = (usuario) => {
+  jwt.sing(
+    {
+      _id: usuario._id,
+      nome: usuario.nome,
+      email: usuario.email,
+      isAdmin: usuario.isAdmin,
+    },
+    process.env.JWT_MENSAGE_SECRETA || 'mensagesecreta'
+  );
+};
