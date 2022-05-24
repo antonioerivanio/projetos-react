e0b5fd001af7a1e1f4c707b9519b19765bd0faf1
antonioerivanio <antonioerivanio@gmail.com>
2022-05-23 23:39:52 -0300

    salvo pedido

--- sistema-agil-impressoes/servidor/rotas/pedidoRouter.js
+++ sistema-agil-impressoes/servidor/rotas/pedidoRouter.js
@@ -1,8 +1,9 @@
 import expressAsyncHandler from 'express-async-handler'
 import Pedido from '../models/pedidoModel';
+import { isAuth } from '../utils/tokens';
 
 const pedidoRouter = express.Router();
-pedidoRouter.post('/', expressAsyncHandler(async(req, res)=>{
+pedidoRouter.post('/', isAuth, expressAsyncHandler(async(req, res)=>{
     if(req.body.itensPedido.length===0){
         res.status(400).send({message: 'Carrinho está vazio'})
 ;    }else{
@@ -13,8 +14,13 @@ pedidoRouter.post('/', expressAsyncHandler(async(req, res)=>{
         precoItens:req.body.precoItens,
         taxaEntrega:req.body.taxaEnvio,
         taxaPreco:req.body.taxaPreco,
-        precoTotal: req.body.precoTotal
+        precoTotal: req.body.precoTotal,
+        usuario:req.usuario_id,
+    });
 
-    })
+    const pedidoCriado = await pedido.save();
+    res.status(201).send({mensage: 'Novo pedido Criado', pedido: pedidoCriado});
 }
 }));
+
+export default pedidoRouter;
--- sistema-agil-impressoes/servidor/utils/tokens.js
+++ sistema-agil-impressoes/servidor/utils/tokens.js
@@ -1,5 +1,24 @@
 import jwt from 'jsonwebtoken';
 
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(authorization) {
+    const token = authorization.slice(7, authorization.length);//Bearer xxxxxx
+    jwt.verify(token, process.env.JWT_MENSAGE_SECRETA || 'somethingsecret', (err, decode) =>{
+      if(err){
+        req.status(401).send({mensage:'Invad Token'})
+      }else{
+        req.usuario = decode;
+        next();
+      }
+    })
+  }else{
+    req.status(401).send({mensage:'Nenhum Token encontrado'})
+  }
+
+  
+};
+
 export const gerarToken = (usuario) => {
   return jwt.sign(
     {
@@ -14,3 +33,5 @@ export const gerarToken = (usuario) => {
     }
   );
 };
+
+
